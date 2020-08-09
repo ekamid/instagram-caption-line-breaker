789afa4233bbd478c855e6ea93284e6caf741a6e
Ebrahim Khalil <ebrahimkhalilamidbd@gmail.com>
2020-08-10 03:18:14 +0600

    Updates

---
+++ precache-manifest.4374d78e23e56522e981c38d5f1eb2a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd65d8f729307080b6663f7e1ae1674c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daf860f94c97b2775259",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "a00a827bfcf684e7eabc",
+    "url": "/static/js/2.9f301624.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f301624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daf860f94c97b2775259",
+    "url": "/static/js/main.f13678c0.chunk.js"
+  },
+  {
+    "revision": "deb0cd62689d9339eb43",
+    "url": "/static/js/runtime-main.057855c3.js"
+  }
+]);
